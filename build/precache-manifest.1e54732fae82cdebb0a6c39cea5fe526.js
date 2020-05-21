b6578d10b09e5783ec7d5c6f5620e3452cf695cb
Oleg Obiukh 2020 <oleg.obiuh.subscribe@gmail.com>
2020-05-21 12:40:53 +0300

    Deploy 1

---
+++ build/precache-manifest.1e54732fae82cdebb0a6c39cea5fe526.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d9dd6e3f87e0cad1981210acbfb05e",
+    "url": "/h-test1/build/index.html"
+  },
+  {
+    "revision": "94f7d0a5c860c7bdedbe",
+    "url": "/h-test1/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba980a6e39ffd5c74487",
+    "url": "/h-test1/build/static/js/2.b1c2a66a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/h-test1/build/static/js/2.b1c2a66a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f7d0a5c860c7bdedbe",
+    "url": "/h-test1/build/static/js/main.e0dc2a74.chunk.js"
+  },
+  {
+    "revision": "dc5b16e7849b97760b2e",
+    "url": "/h-test1/build/static/js/runtime-main.abf65d90.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/h-test1/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
